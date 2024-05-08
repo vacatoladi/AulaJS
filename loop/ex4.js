7f65c7d57eb7925bc7f8aa1515508b81e54d3926
vacatoladi <keltondbv0@gmail.com>
2024-05-08 17:01:59 -0300

    Deixei o Exercicio 4 mais bonitinho

--- loop/ex4.js
+++ loop/ex4.js
@@ -2,11 +2,11 @@
 // qualquer escolhido pelo usuário até o 10
 
 
-let num = parseFloat( prompt("Digite um número de um a 10") )
+let num = parseFloat(prompt("Digite um número para saber sua tabuada até o 10"))
 
 nummax = num * 10
 
 
 for(num2 = num; num2 <= nummax; num2 += num) {
-    console.log(num2)
+    console.log(num + " X " + (num2 / num) + " = " + num2)
 }
