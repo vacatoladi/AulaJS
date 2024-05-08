5dbc778b5d6b85f32c98f71ae41a84d129174548
vacatoladi <keltondbv0@gmail.com>
2024-05-08 16:40:36 -0300

    Exercicio 10 loop

---
+++ loop/ex10.js
@@ -0,0 +1,9 @@
+// Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+
+let num = prompt("Digite um numero")
+
+while(num > 0){
+    num = prompt("Digite um numero")
+}
