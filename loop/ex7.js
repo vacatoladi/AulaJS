e72d321327f6966e98a01137e90c2de4ad24abd6
vacatoladi <keltondbv0@gmail.com>
2024-05-08 15:51:15 -0300

    Exercicio 7 loop

---
+++ loop/ex7.js
@@ -0,0 +1,11 @@
+// Calcular a soma dos números de 1 a 100 utilizando a estrutura while
+
+
+let num = 0
+let num2 = 1
+
+while(num2 <= 101){
+    console.log(num)
+    num = num + num2
+    num2 ++
+}
