19b76caa8e11dfe67f7ce3030e237d15a3a870c3
vacatoladi <keltondbv0@gmail.com>
2024-05-08 16:34:28 -0300

    Exercicio 9 loop

---
+++ loop/ex9.js
@@ -0,0 +1,9 @@
+// Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0
+
+let num = prompt("Digite um numero")
+
+while(num >= 0){
+    console.log(num)
+    num--
+}
